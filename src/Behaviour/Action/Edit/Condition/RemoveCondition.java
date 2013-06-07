fb4563b8d098bb4b362977612636b70ebaefc19c
jappeace <jappieklooster@hotmail.com>
2013-06-07 22:46:36 +0200

    finished the coding for the runtime editing off behavior...
    But this was a large repetative mundane coding task. So i think i will unit test every class i created

--- src/Behaviour/Action/Edit/Condition/RemoveCondition.java
+++ src/Behaviour/Action/Edit/Condition/RemoveCondition.java
@@ -11,12 +11,14 @@ import Behaviour.IBehavior;
  *
  * @author jappie
  */
-public class AddCondition extends ConditionEdit {
-    public AddCondition(IBehavior from, ICondition target){
+public class RemoveCondition extends ConditionEdit{
+    
+    public RemoveCondition(IBehavior from, ICondition target){
 	super(from, target);
     }
+
     public void execute() {
-	getFrom().add(getTarget());
+	getFrom().remove(getTarget());
     }
     
 }
