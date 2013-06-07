b9311642b1f638d115c58c9c6b0dec5a4172ffff
jappeace <jappieklooster@hotmail.com>
2013-06-07 22:58:26 +0200

    added a behavior mock wich records which methods are callod on it & which what variables

---
+++ test/Mock/BehaviorMock.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mock;
+
+import Behaviour.Action.IAction;
+import Behaviour.Condition.ICondition;
+import Behaviour.IBehavior;
+
+/**
+ *
+ * @author jappie
+ */
+public class BehaviorMock implements IBehavior {
+    
+    public IAction _addedAction, _removedAction;
+    public ICondition _addedCondition, _removedCondition;
+    public IBehavior _addedBehavior, _removedBehavior;
+    
+    public boolean 
+	    _isSufficient = true, 
+	    _executionValue = true,
+	    _isExecuted = false;
+
+    public void add(IAction action) {
+	_addedAction = action;
+    }
+
+    public void add(ICondition condition) {
+	_addedCondition = condition;
+    }
+
+    public void add(IBehavior behavior) {
+	_addedBehavior = behavior;
+    }
+
+    public void remove(IAction action) {
+	_removedAction = action;
+    }
+
+    public void remove(ICondition condition) {
+	_removedCondition = condition;
+    }
+
+    public void remove(IBehavior behavior) {
+	_removedBehavior = behavior;
+    }
+
+    public void execute() {
+	_isExecuted = _executionValue;
+    }
+
+    public boolean isSufficient() {
+	return _isSufficient;
+    }
+    
+}
