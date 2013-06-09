c2ec47bd22a11fb6c412960122feb606128e85ae
jappeace <jappieklooster@hotmail.com>
2013-06-09 13:07:34 +0200

    forgot to add the test in last commit

---
+++ test/Test/World/Factory/FactoryTest.java
@@ -0,0 +1,67 @@
+package Test.World.Factory;
+
+import Mock.Behaviour.ConditionMock;
+import Stub.AbstractStub;
+import Test.World.BehaviortestMocks;
+import World.Behaviour.Condition.ICondition;
+import World.Behaviour.Decorator.Abstract.ValidDecorator;
+import World.Factory.Factory;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jappie
+ */
+public class FactoryTest extends BehaviortestMocks{
+    
+    Factory<ICondition> _instanceCondition;
+
+    public FactoryTest() {
+    }
+
+    @Before
+    public void setUp() {
+	System.out.print("FactoryTest test: ");
+	_instanceCondition = new Factory<ICondition>(ConditionMock.class);
+    }
+
+    @Test
+    public void testCreateHappy() {
+	System.out.println("Happy path create");
+	ICondition conditionMock = _instanceCondition.create();
+	assertEquals(conditionMock, new ConditionMock());
+    }
+    @Test
+    public void testCreateCast() {
+	System.out.println("cast test");
+	_instanceCondition.setImplementation(String.class);
+	try{
+	    ICondition conditionMock = _instanceCondition.create();
+	}catch(ClassCastException e){
+	    return;
+	}
+	fail("Factory should not create a string when its type is ICondition");
+    }
+    @Test
+    public void testCreateAbstract() {
+	System.out.println(" abstracct creation");
+	
+	// disable logging because this test generates an ugle mesage in outputs, wich
+	// future me may find anoying
+	Logger.getGlobal().setLevel(Level.OFF);
+	_instanceCondition.setImplementation(AbstractStub.class);
+	
+	Logger.getGlobal().setLevel(Level.ALL);
+	try{
+	    ICondition conditionMock = _instanceCondition.create();
+	}catch(IllegalArgumentException e){
+	    return;
+	}
+	fail("Factory should not create an abstract class, ever!");
+    }
+
+}
