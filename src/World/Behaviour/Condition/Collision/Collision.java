0421792bb21a15e29ae17042e1acf5e59bac6f22
jappeace <jappieklooster@hotmail.com>
2013-06-14 19:46:38 +0200

    added a constructor that allows collision shapes to be listened to... the default spacials uses the most heavy kind of collision shape

--- src/World/Behaviour/Condition/Collision/Collision.java
+++ src/World/Behaviour/Condition/Collision/Collision.java
@@ -7,6 +7,7 @@ package World.Behaviour.Condition.Collision;
 import World.Behaviour.Condition.Condition;
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.collision.PhysicsCollisionObject;
+import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
@@ -20,13 +21,25 @@ import com.jme3.scene.Spatial;
 public class Collision extends Condition {
     CollisionListener _listener;
     
+    public Collision(PhysicsSpace space, CollisionShape shape){
+	register(space, shape);
+    }
+    
     public Collision(PhysicsSpace space, Spatial ... targets){
 	Node n = new Node();
 	for(Spatial component : targets){
 	    n.attachChild(component);
 	}
-	_listener = new CollisionListener(CollisionShapeFactory.createMeshShape(n));
-	space.add((PhysicsCollisionObject)_listener);
+	register(space, CollisionShapeFactory.createMeshShape(n));
+    }
+    /**
+     * registers the shape to the space
+     * @param space
+     * @param shape 
+     */
+    private void register(PhysicsSpace space, CollisionShape shape){
+	_listener = new CollisionListener(shape);
+	space.add((PhysicsCollisionObject)_listener);	
     }
 
     public boolean isSufficient() {
