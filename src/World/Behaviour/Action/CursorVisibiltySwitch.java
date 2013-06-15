94fb3f235e94bf79747306111a08438fbec85d37
jappeace <jappieklooster@hotmail.com>
2013-06-15 13:02:47 +0200

    added an action that changes the cursorVisibility

---
+++ src/World/Behaviour/Action/CursorVisibiltySwitch.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package World.Behaviour.Action;
+
+import com.jme3.input.InputManager;
+
+/**
+ * changes the cursor visibility
+ * @author jappie
+ */
+public class CursorVisibiltySwitch extends Action {
+    private InputManager _target;
+    public CursorVisibiltySwitch(InputManager target){
+	_target = target;
+    }
+    public void execute() {
+	_target.setCursorVisible(!_target.isCursorVisible());
+    }
+    
+}
