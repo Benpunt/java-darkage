ec2be233ec5b2b5f2b4b2c8df7dc2bc38c47bcad
jappeace <jappieklooster@hotmail.com>
2013-06-15 12:48:06 +0200

    moved scene population to a different state

--- src/Engine/Engine.java
+++ src/Engine/Engine.java
@@ -9,20 +9,12 @@ import Engine.Handler.FloatHandler;
 import Engine.Handler.IFloatHandler;
 import UI.BehavioredInput;
 import UI.InputListener;
-import World.Behaviour.Action.Move.Move;
 import World.Behaviour.Action.Stop;
-import World.Behaviour.Behavior;
 import World.Behaviour.IBehavior;
-import World.Factory.Scene.BodyFactory;
-import World.Factory.Scene.ShapeFactory;
-import World.Scene.Visual.Body;
 import com.jme3.app.StatsAppState;
 import com.jme3.bullet.BulletAppState;
-import com.jme3.bullet.PhysicsSpace;
 import com.jme3.input.KeyInput;
-import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,22 +22,23 @@ import java.util.List;
 
 /**
  * This class is an implementation of the vendor engine
- * It handles all my addendums to the world and shows stuff
+ * makes everything tick... basicly it loads states
  * 
  * @author jappie
  */
 public class Engine extends VendorEngine {
-    private BodyFactory _bodyFactory;
     private List<IBehavior> _behaviors;
     private IFloatHandler _tpfHandler;
-    private Body _sillyCubes;
 
     public Engine(){
 	super(
 		new StatsAppState(), // adds some debug texts
 		new CameraState(), // creates camara & keyinput bindings
-		new BulletAppState() // loads physics
+		new BulletAppState(), // loads physics
+		new ScenePopulateState()
 	    );
+	_behaviors = new ArrayList<IBehavior>();
+	_tpfHandler = new FloatHandler();
     }
     @Override
     public void init() {
@@ -57,34 +50,7 @@ public class Engine extends VendorEngine {
 		new BehavioredInput("Escape", new Stop(this), KeyInput.KEY_ESCAPE)
 	);
 	
-	
-	_tpfHandler = new FloatHandler();
-	
 	viewPort.setBackgroundColor(new ColorRGBA(0.7f, 0.8f, 1f, 1f));
-	
-	PhysicsSpace space = stateManager.getState(BulletAppState.class).getPhysicsSpace();
-	
-	ShapeFactory shapeFactory = new ShapeFactory(assetManager);
-	_bodyFactory = new BodyFactory(getRootNode(), shapeFactory);
-	_behaviors = new ArrayList<IBehavior>();
-	
-	Body targetCubes = _bodyFactory.createCubes();
-	
-	
-	_sillyCubes = _bodyFactory.createCubes();
-	
-	_sillyCubes.move(new Vector3f(0f, 1000f, 0f));
-	_behaviors.add(new Behavior(new Move(_sillyCubes, new Vector3f(0f, -10f, 0f), _tpfHandler)));
-	
-
-	 
-	/** Must add a light to make the lit object visible! */
-	DirectionalLight sun = new DirectionalLight();
-	sun.setDirection(new Vector3f(1,0,-2).normalizeLocal());
-	sun.setColor(ColorRGBA.White);
-	rootNode.addLight(sun);
-	
-	createOrientationPoints();
     }
 
     @Override
@@ -96,56 +62,23 @@ public class Engine extends VendorEngine {
      * TODO: make sence of this madness
      */
     public void update(float tpf) {
-	_tpfHandler.set(tpf);
-	for(IBehavior behavior : _behaviors){
+	getTpfHandler().set(tpf);
+	for(IBehavior behavior : getBehaviors()){
 	    behavior.execute();
 	}
     }
-    
-    /** 
-     * i get lost to quickly
-     * TODO: uhh remove?
-     */
-    private void createOrientationPoints(){
-	
-	
-	
-	Body map = _bodyFactory.createMap();
-	map.getNode().move(-2500, -6000f, -2500f);	
-	
-	// aperantly you look at the z axis on the begining
-	Body justSomeCubes = _bodyFactory.createCubes();
-	justSomeCubes.move(new Vector3f(0, 0, 5000f));
-	justSomeCubes.getNode().scale(3f);
-	
-	justSomeCubes = _bodyFactory.createCubes();
-	justSomeCubes.move(new Vector3f(0, 0, -5000f));
-	justSomeCubes.getNode().scale(3f);
 
-	
-	// has to rotate to see it from the center
-    	justSomeCubes = _bodyFactory.createCubes();
-	justSomeCubes.move(new Vector3f(-5000f, 0, 0));
-	justSomeCubes.getNode().rotate(0,(float) (0.5 * Math.PI),0);
-	justSomeCubes.getNode().scale(3f);
-	
-	justSomeCubes = _bodyFactory.createCubes();
-	justSomeCubes.move(new Vector3f(5000f, 0, 0));
-	justSomeCubes.getNode().rotate(0,(float) (0.5 * Math.PI),0);
-	justSomeCubes.getNode().scale(3f);
-	
-	
-	// a nice seeling cross
-	justSomeCubes = _bodyFactory.createCubes();
-	justSomeCubes.move(new Vector3f(0, 5000f, 450));
-	justSomeCubes.getNode().rotate(0,(float) (0.5 * Math.PI),0);
-	justSomeCubes.getNode().scale(3f);
-	
-	justSomeCubes = _bodyFactory.createCubes();
-	justSomeCubes.move(new Vector3f(-450, 5000f, 0));
-	justSomeCubes.getNode().scale(3f);
-	
-	// all those colorfull cubes :)
+    /**
+     * @return the _behaviors
+     */
+    public List<IBehavior> getBehaviors() {
+	return _behaviors;
     }
 
+    /**
+     * @return the _tpfHandler
+     */
+    public IFloatHandler getTpfHandler() {
+	return _tpfHandler;
+    }
 }
---
+++ src/Engine/ScenePopulateState.java
@@ -0,0 +1,116 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Engine;
+
+import World.Behaviour.Action.Move.Move;
+import World.Behaviour.Behavior;
+import World.Factory.Scene.BodyFactory;
+import World.Factory.Scene.ShapeFactory;
+import World.Scene.Visual.Body;
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.light.DirectionalLight;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+
+/**
+ * adds stuff to the scene/world. Initilizes behavior
+ * @author jappie
+ */
+public class ScenePopulateState extends AbstractAppState{
+    
+    private BodyFactory _bodyFactory;
+    private Body _sillyCubes;
+    private Engine _engine;
+    
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+	
+	if(app instanceof Engine){
+	    _engine = (Engine) app;
+	}else{
+	    return;
+	}
+	
+	PhysicsSpace space = stateManager.getState(BulletAppState.class).getPhysicsSpace();
+	
+	ShapeFactory shapeFactory = new ShapeFactory(_engine.getAssetManager());
+	_bodyFactory = new BodyFactory(_engine.getRootNode(), shapeFactory);
+	
+	Body targetCubes = _bodyFactory.createCubes();
+	
+	
+	_sillyCubes = _bodyFactory.createCubes();
+	
+	_sillyCubes.move(new Vector3f(0f, 1000f, 0f));
+	
+
+	 
+	/** Must add a light to make the lit object visible! */
+	DirectionalLight sun = new DirectionalLight();
+	sun.setDirection(new Vector3f(1,0,-2).normalizeLocal());
+	sun.setColor(ColorRGBA.White);
+	_engine.getRootNode().addLight(sun);
+	
+		
+	Body map = _bodyFactory.createMap();
+	map.getNode().move(-2500, -6000f, -2500f);	
+	
+	// aperantly you look at the z axis on the begining
+	Body justSomeCubes = _bodyFactory.createCubes();
+	justSomeCubes.move(new Vector3f(0, 0, 5000f));
+	justSomeCubes.getNode().scale(3f);
+	
+	justSomeCubes = _bodyFactory.createCubes();
+	justSomeCubes.move(new Vector3f(0, 0, -5000f));
+	justSomeCubes.getNode().scale(3f);
+
+	
+	// has to rotate to see it from the center
+    	justSomeCubes = _bodyFactory.createCubes();
+	justSomeCubes.move(new Vector3f(-5000f, 0, 0));
+	justSomeCubes.getNode().rotate(0,(float) (0.5 * Math.PI),0);
+	justSomeCubes.getNode().scale(3f);
+	
+	justSomeCubes = _bodyFactory.createCubes();
+	justSomeCubes.move(new Vector3f(5000f, 0, 0));
+	justSomeCubes.getNode().rotate(0,(float) (0.5 * Math.PI),0);
+	justSomeCubes.getNode().scale(3f);
+	
+	
+	// a nice seeling cross
+	justSomeCubes = _bodyFactory.createCubes();
+	justSomeCubes.move(new Vector3f(0, 5000f, 450));
+	justSomeCubes.getNode().rotate(0,(float) (0.5 * Math.PI),0);
+	justSomeCubes.getNode().scale(3f);
+	
+	justSomeCubes = _bodyFactory.createCubes();
+	justSomeCubes.move(new Vector3f(-450, 5000f, 0));
+	justSomeCubes.getNode().scale(3f);
+	
+	initBehavior();
+    }
+    
+    private void initBehavior(){
+	_engine.getBehaviors().add(
+	    new Behavior(
+		new Move(
+		    _sillyCubes, 
+		    new Vector3f(
+			0f, 
+			-10f, 
+			0f
+		    ), 
+		    _engine.getTpfHandler()
+		)
+	    )
+	);
+	
+    }
+}
