e6a14be57436e694da97aaf65b2d915cc89ab807
jappeace <jappieklooster@hotmail.com>
2013-06-16 12:43:19 +0200

    orginize imports

--- src/World/Behaviour/Condition/Collision/Collision.java
+++ src/World/Behaviour/Condition/Collision/Collision.java
@@ -4,17 +4,12 @@
  */
 package World.Behaviour.Condition.Collision;
 
-import Engine.Handler.IReadFloat;
 import Engine.Log;
 import World.Behaviour.Condition.Condition;
 import World.Scene.ISpatialAcces;
-import World.Scene.Visual.Body;
 import com.jme3.bullet.PhysicsSpace;
-import com.jme3.bullet.collision.PhysicsCollisionObject;
-import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.bullet.util.CollisionShapeFactory;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  * This setup is the fastest i could think off
  * every spacial gets its own listener. The listener returns true if it colides & resets itself
@@ -36,7 +31,7 @@ public class Collision extends Condition {
     }
 
     public boolean isSufficient() {
-	_listener.setSpatial(_target.getSpatial());
+	_listener.setSpatial(_target.getSpatial()); // update the ghost controll
 	boolean colides = _listener.HasColided();
 	
 	Log.write(Level.FINEST, "{1} is colliding: {0}. Overlapping count: {2}", 
